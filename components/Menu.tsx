399115fc650d855f87ddec06a2b72c3cc37d6df4
Tomáš Connor <tomasconnor.cz@gmail.com>
2024-11-10 10:04:16 +0100

    fix menu hiding on link click

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 import Link from "next/link";
 
@@ -6,6 +8,7 @@ import {
   AlertDialog,
   AlertDialogCancel,
   AlertDialogContent,
+  AlertDialogDescription,
   AlertDialogTrigger,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
@@ -16,8 +19,10 @@ import { FadeText } from "@/components/ui/fade-text";
 import { Text, X } from "lucide-react";
 
 const Menu: React.FC = (): React.JSX.Element => {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
-    <AlertDialog>
+    <AlertDialog open={open} onOpenChange={setOpen}>
       <AlertDialogTrigger className="dark:focus-visible:outline-none" asChild>
         <Button variant="ghost" className="[&_svg]:size-5 h-12">
           <Text />
@@ -26,13 +31,18 @@ const Menu: React.FC = (): React.JSX.Element => {
 
       <AlertDialogContent className="flex flex-col items-center justify-between min-h-full min-w-full dark:border-none p-6">
         <AlertDialogTitle className="hidden" /> {/* Warning fix */}
+        <AlertDialogDescription className="hidden" /> {/* Warning fix */}
         <header className="flex justify-end w-full">
           <AlertDialogCancel className="border-none [&_svg]:size-5 h-12">
             <X />
           </AlertDialogCancel>
         </header>
         <nav className="flex flex-col text-center text-2xl gap-4">
-          <Link href="/privacy" className="hover:opacity-85">
+          <Link
+            href="/privacy"
+            onClick={() => setOpen(false)}
+            className="hover:opacity-85"
+          >
             <FadeText
               direction="up"
               framerProps={{
@@ -42,7 +52,11 @@ const Menu: React.FC = (): React.JSX.Element => {
             />
           </Link>
 
-          <Link href="/terms" className="hover:opacity-85">
+          <Link
+            href="/terms"
+            onClick={() => setOpen(false)}
+            className="hover:opacity-85"
+          >
             <FadeText
               direction="up"
               framerProps={{
@@ -52,7 +66,11 @@ const Menu: React.FC = (): React.JSX.Element => {
             />
           </Link>
 
-          <Link href="/contact" className="hover:opacity-85">
+          <Link
+            href="/contact"
+            onClick={() => setOpen(false)}
+            className="hover:opacity-85"
+          >
             <FadeText
               direction="up"
               framerProps={{
