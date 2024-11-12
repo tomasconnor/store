c86f508074f0bb955da98e494a081baa5ef5ab4a
Tomáš Connor <tomasconnor.cz@gmail.com>
2024-11-12 16:41:59 +0100

    add bundle link to top bar text

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Link from "next/link";
 
 const TopBar: React.FC = (): React.JSX.Element => {
   return (
     <div className="flex items-center justify-center bg-white p-2">
       <p className="text-sm uppercase text-center text-black">
-        Free Shipping on Orders Over $20 Plus a Gift in Every Bundle!
+        Free Shipping on Orders Over $20 Plus a Gift in Every{" "}
+        <Link className="underline hover:no-underline" href="/bundle">
+          Bundle
+        </Link>
+        !
       </p>
     </div>
   );
